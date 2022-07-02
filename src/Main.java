e0ee73e23ea5fed5da8d503edfbeb69581e7e4b2
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2022-07-01 23:23:15 -0300

    Inserção dos arquivos para a operação soma.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        InetSocketAddress endereco = new InetSocketAddress(8888);
+
+        HttpServer servidor = HttpServer.create(endereco, 10);
+        servidor.createContext("/soma", new Soma());
+        servidor.start();
+
+        System.out.println("Servidor iniciado ouvindo a port " + endereco.getPort());
+    }
+}
