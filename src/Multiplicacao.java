3571e2c9794498f8066539056d9e06e05999774e
pedrovnogueiras <pvns1@aluno.ifal.edu.br>
2022-07-01 23:52:19 -0300

    Arquivos multiplicação

--- src/Multiplicacao.java
+++ src/Multiplicacao.java
@@ -6,15 +6,14 @@ import java.util.List;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-public class Soma implements HttpHandler {
+public class Multiplicacao implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         try {
             String[] partes = exchange.getRequestURI().getPath().split("/");
             List<Double> numeros = new ArrayList<Double>();
-            double soma = 0;
+            double multiplicacao = 0;
 
-            //Verifica se, na terceira posição da URL, o valor é igual a null, haja vista que, caso seja válido, a aplicação deve retornar uma mensagem de "Entrada Inválida".  
             if(partes[2].equals(null)) {
                 byte[] resposta = "Entrada inválida".getBytes();
                 exchange.sendResponseHeaders(200, resposta.length);
@@ -22,7 +21,6 @@ public class Soma implements HttpHandler {
                 return;
             }
 
-            //Verifica se o(s) caractere(s) inserido(s) são, de fato, númericos e, em caso de confirmação, o número é adicionado ao array de números para, posteriormente, realizar a operação soma. 
             for(int i = 2; i < partes.length; i++) {
                 boolean isNumeric = checkNum(partes[i]);
                 if(isNumeric) {
@@ -36,12 +34,11 @@ public class Soma implements HttpHandler {
                 }
             } 
 
-            //A variável soma recebe os valores de cada posição do array de números, sendo inseridos na variável soma.
             for(int i = 0; i < numeros.size(); i++) {
-                soma += numeros.get(i);
+                multiplicacao = multiplicacao*numeros.get(i);
             }
 
-            byte[] resposta = Double.toString(soma).getBytes();
+            byte[] resposta = Double.toString(multiplicacao).getBytes();
             exchange.sendResponseHeaders(200, resposta.length);
             exchange.getResponseBody().write(resposta);
         } catch(Exception e) {
@@ -51,7 +48,6 @@ public class Soma implements HttpHandler {
         }
     }
     
-    //Função destinada à checagem do número.
     private boolean checkNum(String strNum) {
         boolean isNumeric = true;
 
