013a8c206e77f0e5ed05af2c701e7bbfe3435a87
Pedro Henriquer <phbb1@aluno.ifal.edu.br>
2022-07-27 17:39:28 -0300

    Inserção do primeiro arquivo .java.

--- api/src/main/java/app/App.java
+++ api/src/main/java/app/App.java
@@ -5,7 +5,7 @@ import io.jooby.Jooby;
 public class App extends Jooby {
 
   {
-    get("/", ctx -> "Welcome to Jooby!");
+    mvc(new Index());
   }
 
   public static void main(final String[] args) {
