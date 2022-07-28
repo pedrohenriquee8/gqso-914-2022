08831526da8986b1c29fa7e3978907d44953b2ab
Pedro Henriquer <phbb1@aluno.ifal.edu.br>
2022-07-27 23:56:39 -0300

    Operação subtração da Calculadora API.

--- api/src/main/java/app/Subtracao.java
+++ api/src/main/java/app/Subtracao.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.util.Arrays;
+
 import io.jooby.annotations.*;
 
 @Path("/subtracao/{nums}*")
@@ -8,9 +9,13 @@ public class Subtracao {
 
     @GET
     public double calculoSubtracao(@PathParam String nums) {
+        double maxDouble = Arrays.stream(nums.split("/"))
+                .map(num -> Double.parseDouble(num))
+                .reduce((double) 0, Double::max);
+
         return Arrays.stream(nums.split("/"))
                 .map(num -> Double.parseDouble(num))
-                .reduce(0.0, (a, b) -> a - b);
+                .reduce(maxDouble, (a, b) -> a - b);
     }
 
 }
