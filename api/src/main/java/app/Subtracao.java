4fd8761ccf7dd1db8e476103c6e698a0c1890e04
pedrohenriquee8 <phbb1@aluno.ifal.edu.br>
2022-08-09 20:36:09 +0000

    Detalhamento final.

--- api/src/main/java/app/Subtracao.java
+++ api/src/main/java/app/Subtracao.java
@@ -1,7 +1,5 @@
 package app;
 
-import java.util.ArrayList;
-
 import io.jooby.annotations.*;
 
 @Path("/subtracao/{nums}*")
@@ -10,18 +8,13 @@ public class Subtracao {
     @GET
     public double calculoSubtracao(@PathParam String nums) {
         String[] numsParam = nums.split("/");
-        ArrayList<Double> numsDouble = new ArrayList<>();
         double parametroSoma = 0;
 
-        for(int i = 0; i < numsParam.length; i++) {
-            numsDouble.add(Double.parseDouble(numsParam[i]));
+        for(int i = 1; i < numsParam.length; i++) {
+            parametroSoma += Double.parseDouble(numsParam[i]);
         }
 
-        for(int i = 1; i < numsDouble.size(); i++) {
-            parametroSoma = parametroSoma + numsDouble.get(i);
-        }
-
-        return numsDouble.get(0) - parametroSoma;
+        return Double.parseDouble(numsParam[0]) - parametroSoma;
     }
 
 }
